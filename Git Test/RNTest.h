e4f0e3dfaca8760950492ff57ef8ca4f524064d2
Richard Nolet <Richard.Nolet@gee.com>
2014-04-16 00:34:02 -0400

    added a Test Class with new personal info

---
+++ Git Test/RNTest.h
@@ -0,0 +1,13 @@
+//
+//  RNTest.h
+//  Git Test
+//
+//  Created by Ricardo Nolet on 2014/04/16/.
+//  Copyright (c) 2014 Ricardo Nolet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNTest : NSObject
+
+@end
---
+++ Git Test/RNTest.m
@@ -0,0 +1,13 @@
+//
+//  RNTest.m
+//  Git Test
+//
+//  Created by Ricardo Nolet on 2014/04/16/.
+//  Copyright (c) 2014 Ricardo Nolet. All rights reserved.
+//
+
+#import "RNTest.h"
+
+@implementation RNTest
+
+@end
